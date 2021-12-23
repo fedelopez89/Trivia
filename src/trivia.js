81d8e7396d52125d0cecd52bf6fa17ba4551e7ee
Federico López <flopez@science37.com>
2021-12-23 13:52:25 -0300

    add more questions

--- src/trivia.js
+++ src/trivia.js
@@ -17,13 +17,35 @@ const trivia = {
       text: "Pregunta 2",
       image:
         "https://filedn.com/ltOdFv1aqz1YIFhf4gTY8D7/ingus-info/BLOGS/Photography-stocks3/stock-photography-slider.jpg",
-      lifetimeSeconds: 5,
+      lifetimeSeconds: 10,
       options: [
         { text: "Opcion1", correct: true },
         { text: "Opcion2", correct: false },
         { text: "Opcion3", correct: false },
       ],
     },
+    {
+      text: "Pregunta 3",
+      image:
+        "https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg",
+      lifetimeSeconds: 10,
+      options: [
+        { text: "Opcion1", correct: false },
+        { text: "Opcion2", correct: false },
+        { text: "Opcion3", correct: true },
+      ],
+    },
+    {
+      text: "Pregunta 4",
+      image:
+        "https://filedn.com/ltOdFv1aqz1YIFhf4gTY8D7/ingus-info/BLOGS/Photography-stocks3/stock-photography-slider.jpg",
+      lifetimeSeconds: 10,
+      options: [
+        { text: "Opcion1", correct: true },
+        { text: "Opcion2", correct: false },
+        { text: "Opcion3", correct: false },
+      ],
+    },    
   ],
 };
 
